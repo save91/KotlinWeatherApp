6e58d88586db54e06536ee186409735c8cce4d7f
Saverio Tosi <s.tosi@extrategy.net>
2017-09-20 22:46:19 +0200

    add a simple list

--- app/src/main/java/net/extrategy/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/net/extrategy/kotlinweatherapp/MainActivity.kt
@@ -2,13 +2,27 @@ package net.extrategy.kotlinweatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val items = listOf(
+            "Lunedì - Pioggia - min 15 - max 34",
+            "Martedì - Pioggia - min 15 - max 34",
+            "Mercoledì - Pioggia - min 15 - max 34",
+            "Giovedì - Pioggia - min 15 - max 34",
+            "Venerdì - Pioggia - min 15 - max 34",
+            "Sabato - Pioggia - min 15 - max 34",
+            "Domenica - Pioggia - min 15 - max 34"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!!"
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
     }
 }
