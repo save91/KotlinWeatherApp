76b8201d26ea59e477cf27ac5fa1b1e32576c9a2
Saverio Tosi <s.tosi@extrategy.net>
2017-09-21 21:31:08 +0200

    an example of async request

--- app/src/main/java/net/extrategy/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/net/extrategy/kotlinweatherapp/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
+    private val url = "http://app.e-xtrategy.net/demo/weather/today.json"
     private val items = listOf(
             "Lunedì - Pioggia - min 15 - max 34",
             "Martedì - Pioggia - min 15 - max 34",
@@ -17,12 +21,17 @@ class MainActivity : AppCompatActivity() {
             "Domenica - Pioggia - min 15 - max 34"
     )
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
 
     }
 }
