2adfaa43533f7a1100b8ac187c0c43f4b37242e4
Saverio Tosi <s.tosi@extrategy.net>
2017-09-21 22:47:11 +0200

    add data class for service result

---
+++ app/src/main/java/net/extrategy/kotlinweatherapp/data/ResponseClasses.kt
@@ -0,0 +1,37 @@
+package net.extrategy.kotlinweatherapp.data
+
+data class ForecastResult(
+        val city: City,
+        val cod: Int,
+        val message: Float,
+        val cnt: Int,
+        val list: List<Forecast>)
+
+data class City(
+        val id: Long,
+        val name: String,
+        val coord: Coordinates,
+        val country: String,
+        val population: Int)
+
+data class Coordinates(val lon: Float, val lat: Float)
+
+data class Forecast(
+        val dt: Long,
+        val temp: Temperature,
+        val pressure: Float,
+        val humidity: Int,
+        val weather: List<Weather>,
+        val speed: Float,
+        val deg: Int,
+        val clouds: Int)
+
+data class Temperature(
+        val day: Float,
+        val min: Float,
+        val max: Float,
+        val night: Float,
+        val eve: Float,
+        val morn: Float)
+
+data class Weather(val id: Long, val main: String, val description: String,val icon: String)
