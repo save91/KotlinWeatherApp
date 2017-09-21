1b3cf9bbd764d663adf46ccfe1da751ed5133ba8
Saverio Tosi <s.tosi@extrategy.net>
2017-09-21 21:20:19 +0200

    add class Request for HTTP request

---
+++ app/src/main/java/net/extrategy/kotlinweatherapp/Request.kt
@@ -0,0 +1,11 @@
+package net.extrategy.kotlinweatherapp
+
+import android.util.Log
+import java.net.URL
+
+class Request(private val url: String) {
+    fun run() {
+        val forecastJsonStr = URL(url).readText()
+        Log.d(javaClass.simpleName, forecastJsonStr)
+    }
+}
