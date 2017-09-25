16bf581d49fcddc20a4c46ba69a8fdf437812e0a
Saverio Tosi <s.tosi@extrategy.net>
2017-09-25 22:26:33 +0200

    simplify with kotlin android extension

--- app/src/main/java/net/extrategy/kotlinweatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/net/extrategy/kotlinweatherapp/ui/activities/MainActivity.kt
@@ -3,25 +3,20 @@ package net.extrategy.kotlinweatherapp.ui.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import net.extrategy.kotlinweatherapp.R
 import net.extrategy.kotlinweatherapp.domain.commands.RequestForecastCommand
-import net.extrategy.kotlinweatherapp.domain.model.Forecast
 import net.extrategy.kotlinweatherapp.ui.adapters.ForecastListAdapter
-import org.jetbrains.anko.custom.async
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.longToast
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
-
         doAsync {
             val result = RequestForecastCommand().execute()
             uiThread {
--- app/src/main/java/net/extrategy/kotlinweatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/net/extrategy/kotlinweatherapp/ui/adapters/ForecastListAdapter.kt
@@ -4,13 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
 import net.extrategy.kotlinweatherapp.R
 import net.extrategy.kotlinweatherapp.domain.model.Forecast
 import net.extrategy.kotlinweatherapp.domain.model.ForecastList
-import org.jetbrains.anko.find
 
 class ForecastListAdapter(
         private val forecast: ForecastList,
@@ -30,19 +28,14 @@ class ForecastListAdapter(
     override fun getItemCount(): Int = forecast.size
 
     class ViewHolder(view: View, private val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTView = view.find<TextView>(R.id.maxTemperature)
-        private val minTView = view.find<TextView>(R.id.minTemperature)
 
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.context).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTView.text = "$high°"
-                minTView.text = "$low°"
+                Picasso.with(itemView.context).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "$high°"
+                itemView.minTemperature.text = "$low°"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
