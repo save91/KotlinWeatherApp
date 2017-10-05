65ade91af37d4303cd14f82bd5d10e178fc339a2
Saverio Tosi <s.tosi@extrategy.net>
2017-10-05 21:54:42 +0200

    add singleton for app

---
+++ app/src/main/java/net/extrategy/kotlinweatherapp/App.kt
@@ -0,0 +1,15 @@
+package net.extrategy.kotlinweatherapp
+
+import android.app.Application
+
+class App : Application() {
+
+    companion object {
+        lateinit var instance: App
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
