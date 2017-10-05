383d151647c237be6299ee1a4cbaa28e1f84f301
Saverio Tosi <s.tosi@extrategy.net>
2017-10-05 22:23:03 +0200

    add database helper

---
+++ app/src/main/java/net/extrategy/kotlinweatherapp/data/db/ForecastDbHelper.kt
@@ -0,0 +1,41 @@
+package net.extrategy.kotlinweatherapp.data.db
+
+import android.database.sqlite.SQLiteDatabase
+import net.extrategy.kotlinweatherapp.App
+import org.jetbrains.anko.db.*
+
+class ForecastDbHelper() : ManagedSQLiteOpenHelper(App.instance,
+        ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance by lazy {
+            ForecastDbHelper()
+        }
+    }
+
+    override fun onConfigure(db: SQLiteDatabase) {
+        db.createTable(
+                CityForecastTable.NAME,
+                true,
+                CityForecastTable.ID to INTEGER + PRIMARY_KEY,
+                CityForecastTable.CITY to TEXT,
+                CityForecastTable.COUNTRY to TEXT)
+
+        db.createTable(DayForecastTable.NAME, true,
+                DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                DayForecastTable.DATE to INTEGER,
+                DayForecastTable.DESCRIPTION to TEXT,
+                DayForecastTable.HIGH to INTEGER,
+                DayForecastTable.LOW to INTEGER,
+                DayForecastTable.ICON_URL to TEXT,
+                DayForecastTable.CITY_ID to INTEGER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(CityForecastTable.NAME, true)
+        db.dropTable(DayForecastTable.NAME, true)
+        onCreate(db)
+    }
+}
